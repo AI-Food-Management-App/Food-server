5579bd3afa334ee1c11fe7f873270dbc19687c71
GemmaRyan <157807096+GemmaRyan@users.noreply.github.com>
2026-03-07 00:07:14 +0000

    Changed index.mjs to remove localhost and the env for the ml render route = https://python-backend-d6b9.onrender.com

--- index.mjs
+++ index.mjs
@@ -13,13 +13,28 @@ import { supabase } from "./db/supabase.mjs";
 const app = express();
 const PORT = process.env.PORT || 5050;
 
-app.use(cors({ origin: ["http://localhost:4200"], credentials: true }));
+//app.use(cors({ origin: ["http://localhost:4200"], credentials: true }));
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(bodyParser.json());
 
 app.use("/api", shoppingListRoutes);
 app.use("/api", fridgeRoutes);
 app.use("/api", mlRoutes);
+
+//added to check render server
+app.get("/", (_req, res) => {
+  res.json({ ok: true, message: "Food server is running" });
+});
+
+app.get("/health", (_req, res) => {
+  res.json({ ok: true });
+});
 
 (async () => {
   try {
