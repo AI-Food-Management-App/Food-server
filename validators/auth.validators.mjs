486133ae4399256a5c3946a1a805b25c3dd2e73b
Elain Polakova - STUDENT <S00250500@atu.ie>
2026-03-12 21:16:28 +0000

    added a trim

--- validators/auth.validators.mjs
+++ validators/auth.validators.mjs
@@ -33,7 +33,7 @@ export function validateEmail(email) {
 }
 // Password
 export function validatePassword(password) {
-  if (!password)
+  if (!password || !password.trim())
     return "Password is required";
   if (password.length < 8)
     return "Password must be at least 8 characters";
