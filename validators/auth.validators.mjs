66be5447dacc2aa0c574ea8f35448b5fe5b7a6d2
GemmaRyan <157807096+GemmaRyan@users.noreply.github.com>
2026-03-12 21:46:56 +0000

    commenting out changes

--- validators/auth.validators.mjs
+++ validators/auth.validators.mjs
@@ -33,7 +33,8 @@ export function validateEmail(email) {
 }
 // Password
 export function validatePassword(password) {
-  if (!password || !password.trim())
+  //if (!password || !password.trim())
+  if (!password)
     return "Password is required";
   if (password.length < 8)
     return "Password must be at least 8 characters";
